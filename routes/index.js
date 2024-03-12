cf6ee957542b4c49c5b9c23f86af986e9b9de900
Patrick-052 <muiruripatrick052@gmail.com>
2024-03-12 21:17:09 +0300

    Get and List file

--- routes/index.js
+++ routes/index.js
@@ -20,9 +20,9 @@ router.get('/users/me', UsersController.getMe);
 
 router.post('/files', FilesController.postUpload);
 
-// router.get('/files/:id', FilesController.getShow);
+router.get('/files/:id', FilesController.getShow);
 
-// router.get('/files', FilesController.getIndex);
+router.get('/files', FilesController.getIndex);
 
 // router.put('/files/:id/publish', FilesController.putPublish);
 
