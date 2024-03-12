14ce68b485f7c7136ac3ca830de035f0c39855bc
Patrick-052 <muiruripatrick052@gmail.com>
2024-03-12 21:21:00 +0300

    File data

--- routes/index.js
+++ routes/index.js
@@ -28,6 +28,6 @@ router.put('/files/:id/publish', FilesController.putPublish);
 
 router.put('/files/:id/unpublish', FilesController.putUnpublish);
 
-// router.get('/files/:id/data', FilesController.getFile);
+router.get('/files/:id/data', FilesController.getFile);
 
 module.exports = router;
