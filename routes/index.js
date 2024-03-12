664dfa2afffdc1668489a45cfe370aad82b240f0
Patrick-052 <muiruripatrick052@gmail.com>
2024-03-12 21:06:18 +0300

    First Api

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 import { Router } from 'express';
 import AppController from '../controllers/AppController';
-import UsersController from '../controllers/UsersController';
-import AuthController from '../controllers/AuthController';
-import FilesController from '../controllers/FilesController';
+// import UsersController from '../controllers/UsersController';
+// import AuthController from '../controllers/AuthController';
+// import FilesController from '../controllers/FilesController';
 
 const router = Router();
 
@@ -10,24 +10,24 @@ router.get('/status', AppController.getStatus);
 
 router.get('/stats', AppController.getStats);
 
-router.post('/users', UsersController.postNew);
+// router.post('/users', UsersController.postNew);
 
-router.get('/connect', AuthController.getConnect);
+// router.get('/connect', AuthController.getConnect);
 
-router.get('/disconnect', AuthController.getDisconnect);
+// router.get('/disconnect', AuthController.getDisconnect);
 
-router.get('/users/me', UsersController.getMe);
+// router.get('/users/me', UsersController.getMe);
 
-router.post('/files', FilesController.postUpload);
+// router.post('/files', FilesController.postUpload);
 
-router.get('/files/:id', FilesController.getShow);
+// router.get('/files/:id', FilesController.getShow);
 
-router.get('/files', FilesController.getIndex);
+// router.get('/files', FilesController.getIndex);
 
-router.put('/files/:id/publish', FilesController.putPublish);
+// router.put('/files/:id/publish', FilesController.putPublish);
 
-router.put('/files/:id/unpublish', FilesController.putUnpublish);
+// router.put('/files/:id/unpublish', FilesController.putUnpublish);
 
-router.get('/files/:id/data', FilesController.getFile);
+// router.get('/files/:id/data', FilesController.getFile);
 
 module.exports = router;
