cd3dcf1ac564cb49050a05ed0133b1060576e15b
Patrick-052 <muiruripatrick052@gmail.com>
2024-03-12 21:13:01 +0300

    Authenticating a user

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
-// import AuthController from '../controllers/AuthController';
+import AuthController from '../controllers/AuthController';
 // import FilesController from '../controllers/FilesController';
 
 const router = Router();
@@ -12,11 +12,11 @@ router.get('/stats', AppController.getStats);
 
 router.post('/users', UsersController.postNew);
 
-// router.get('/connect', AuthController.getConnect);
+router.get('/connect', AuthController.getConnect);
 
-// router.get('/disconnect', AuthController.getDisconnect);
+router.get('/disconnect', AuthController.getDisconnect);
 
-// router.get('/users/me', UsersController.getMe);
+router.get('/users/me', UsersController.getMe);
 
 // router.post('/files', FilesController.postUpload);
 
